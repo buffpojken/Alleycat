5a4a622057a3463fe11d6d6708dc869083fc6dd6
buffpojken <daniel@sykewarrior.com>
2011-03-24 22:39:18 +0100

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alleycat::Application.config.secret_token = 'ae3cf0e7f18b175922bd9acbc0757e01fb56a7cbf2b85854bef012bf5d0fac72c0fd255b97329680a634ddc0dfedf4a392a1f64acd4d97b7d072a5de741ebe60'
